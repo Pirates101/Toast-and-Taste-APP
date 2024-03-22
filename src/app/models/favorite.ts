d2ea884d44ca867d11e2e7c06d0dc6b951cdd102
Aimee Rzepka <rzepka.aimee@gmail.com>
2024-03-21 21:12:32 -0400

    added favorites component

--- src/app/models/favorite.ts
+++ src/app/models/favorite.ts
@@ -1,2 +1,12 @@
 export interface Favorite {
+    id: number,
+    markedDate: Date,
+    cheese: string,
+    wine: string,
 }
+export interface PostFavoriteModel {
+    id: number,
+    markedDate: Date, 
+    cheese: string,
+    wine: string, 
+  }
--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Favorite, PostFavoriteModel } from "../models/favorite";
+import { Injectable } from '@angular/core'
+import { environment } from "../../environments/environment.development";
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  baseUrl = `${environment.apiDomain}/api/Favorites`;
+
+  addFavorite(favorite: PostFavoriteModel): Observable<Favorite> {
+    return this.httpClient.post<Favorite>(this.baseUrl, favorite); 
+  }
+
+  getFavorites() {
+    return this.httpClient.get<Favorite[]>(this.baseUrl);
+  }
 }
