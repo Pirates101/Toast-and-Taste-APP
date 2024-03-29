0c71d9db63afbba08781c6ee87b173724fc0d97f
actonlauren <actonlauren@yahoo.com>
2024-03-28 22:04:53 -0400

    added trash can for removing from favorites, added properties to read in combo data

---
+++ src/app/models/user-combinations.ts
@@ -0,0 +1,13 @@
+export interface Combos {
+    id: number;
+    userid: number;
+    wine: string;
+    cheese: string;
+}
+
+export interface SubmitCombos {
+    id: number;
+    userid: number;
+    wine: string;
+    cheese: string;
+}
