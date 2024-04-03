b6a518d25ab41671c7c7515f51882f82fde4adf4
Peggy Sabido <peggy_sabido@hotmail.com>
2024-04-03 18:55:47 -0400

    FixingEnv

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-	apiDomain: 'https://toastandtaste20240328213226.azurewebsites.net"'
+	apiDomain: 'https://toastandtaste20240328213226.azurewebsites.net'
 };
